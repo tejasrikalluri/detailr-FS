eec04295dc48c61c68175d573a2dcd508628bf63
tejasrikalluri <tkalluri@artissol.com>
2022-06-11 21:36:36 +0530

    first commit

---
+++ app/scripts/utils.js
@@ -0,0 +1,58 @@
+function isISODate(val) {
+  if (
+    /(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})[+-](\d{2})\:(\d{2})/.test(
+      val
+    )
+  ) {
+    return true;
+  }
+  if (/(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})[Z]/.test(val)) {
+    return true;
+  }
+}
+
+function formatArrayString(val) {
+  return val.join(", ");
+}
+
+function formatDate(val) {
+  return moment(val).format("MMM Do YYYY");
+  // return val;
+}
+
+//formats custom fields
+function formatCustomFields(values, cust_field, custArr) {
+  for (let i = 0; i < cust_field.length; i++) {
+    let key = cust_field[i];
+    let val;
+    if (
+      values[key] != undefined &&
+      values[key] != null &&
+      values[key] != "" &&
+      values[key] != " "
+    ) {
+      val = values[key];
+    } else {
+      val = "N/A";
+    }
+    custArr.push(
+      '<div><span class="muted ucwords" style="color:red !important">' +
+        key.replace("_", " ") +
+        "</span>",
+      ": ",
+      '<span class="muted ucwords" style="color:green !important">' +
+        xss_test(val) +
+        "</span>",
+      "<br/></div>"
+    );
+  }
+  return custArr.join("");
+}
+
+function xss_test(name) {
+  return $("<span></span>").text(name)[0].innerHTML;
+}
+
+function isValNotEmpty(val) {
+  return !!val && val.toString().trim().length > 0;
+}
---
+++ server/server.js
@@ -0,0 +1,167 @@
+var base64 = require("base-64");
+
+exports = {
+  searchAgent: function (args) {
+    var domain = args.iparams.domain;
+    var apiKey = args.iparams.api_key;
+    var email = base64.decode(args.email);
+    var url = "https://" + domain + "/api/v2/agents?email=" + email;
+    var headers = {
+      Authorization: "Basic " + base64.encode(apiKey + ":X"),
+      "Content-Type": "application/json",
+    };
+    var reqData = {
+      headers: headers,
+    };
+    $request.get(url, reqData).then(
+      function (data) {
+        var result = JSON.parse(data.response).agents;
+        if (result.length === 0) {
+          let requiredData = {};
+          requiredData["result"] = undefined;
+          renderData(null, {
+            finalResult: requiredData,
+          });
+        } else {
+          getContactCustomFieldsLabel(args, result);
+        }
+      },
+      function () {
+        renderData(null, {
+          finalResult: null,
+        });
+      }
+    );
+  },
+  searchRequester: function (args) {
+    var domain = args.iparams.domain;
+    var apiKey = args.iparams.api_key;
+    var email = base64.decode(args.email);
+    var url = "https://" + domain + "/api/v2/requesters?email=" + email;
+    var headers = {
+      Authorization: "Basic " + base64.encode(apiKey + ":X"),
+      "Content-Type": "application/json",
+    };
+    var reqData = {
+      headers: headers,
+    };
+    $request.get(url, reqData).then(
+      function (data) {
+        var result = JSON.parse(data.response).requesters;
+        if (result.length === 0) {
+          let requiredData = {};
+          requiredData["result"] = undefined;
+          renderData(null, {
+            finalResult: requiredData,
+          });
+        } else {
+          getContactCustomFieldsLabel(args, result);
+        }
+      },
+      function () {
+        renderData(null, {
+          finalResult: null,
+        });
+      }
+    );
+  },
+  getDepartment: function (args) {
+    var domain = args.iparams.domain;
+    var apiKey = args.iparams.api_key;
+    var id = base64.decode(args.id);
+    var url = "https://" + domain + "/api/v2/departments/" + id;
+    var headers = {
+      Authorization: "Basic " + base64.encode(apiKey + ":X"),
+      "Content-Type": "application/json",
+    };
+    var reqData = {
+      headers: headers,
+    };
+    $request.get(url, reqData).then(
+      function (data) {
+        var result = JSON.parse(data.response);
+        var name = result.department.name;
+        renderData(null, {
+          result: name,
+        });
+      },
+      function () {
+        renderData(null, {
+          finalResult: null,
+        });
+      }
+    );
+  },
+  getLocation: function (args) {
+    var domain = args.iparams.domain;
+    var apiKey = args.iparams.api_key;
+    var id = base64.decode(args.id);
+    var url = "https://" + domain + "/api/v2/locations/" + id;
+    var headers = {
+      Authorization: "Basic " + base64.encode(apiKey + ":X"),
+      "Content-Type": "application/json",
+    };
+    var reqData = {
+      headers: headers,
+    };
+    $request.get(url, reqData).then(
+      function (data) {
+        var result = JSON.parse(data.response);
+        var name = result.location.name;
+        renderData(null, {
+          result: name,
+        });
+      },
+      function () {
+        renderData(null, {
+          finalResult: null,
+        });
+      }
+    );
+  },
+};
+
+function getContactCustomFieldsLabel(args, result) {
+  var requiredData = {};
+  var domain = args.iparams.domain;
+  var apiKey = args.iparams.api_key;
+  var personDetails = result[0];
+  var url = "https://" + domain + "/api/v2/requester_fields";
+  var headers = {
+    Authorization: "Basic " + base64.encode(apiKey + ":X"),
+    "Content-Type": "application/json",
+  };
+  var reqData = {
+    headers: headers,
+  };
+  $request.get(url, reqData).then(
+    function (data) {
+      var result = JSON.parse(data.response).requester_fields;
+      var labelValueObj = {};
+      for (let i = 0; i < result.length; i++) {
+        if (result[i].default === false) {
+          let contactFieldVal = personDetails.custom_fields[result[i].name];
+          if (
+            contactFieldVal !== null &&
+            contactFieldVal !== undefined &&
+            contactFieldVal !== ""
+          ) {
+            labelValueObj[result[i].label] = contactFieldVal;
+          } else {
+            labelValueObj[result[i].label] = "N/A";
+          }
+        }
+      }
+      requiredData["result"] = personDetails;
+      requiredData["labelValueObj"] = labelValueObj;
+      renderData(null, {
+        finalResult: requiredData,
+      });
+    },
+    function () {
+      renderData(null, {
+        finalResult: null,
+      });
+    }
+  );
+}
